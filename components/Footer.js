f78e7914b0b91e2e1921c024af87b41e39986f71
Michael Gregoire <mgregoire@nvzion.com>
2019-01-06 18:20:36 -0500

    Port mockup to app

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer>
+    <p>&copy; 2018 ChannelZero.net, All rights reserved</p>
+  </footer>
+);
+
+export default Footer;
---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <style>{`body { margin: 0 }`}</style>
+        </Head>
+        <body className="channel-zero">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
