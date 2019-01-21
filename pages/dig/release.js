86f7eb65b7e1d05326fe057ee6e1b14c57dba586
Michael Gregoire <mgregoire@nvzion.com>
2019-01-21 16:42:41 -0500

    Begin fleshing out dig/release page

--- pages/dig/release.js
+++ pages/dig/release.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Markdown from "markdown-to-jsx";
 import { withRouter } from "next/router";
 import DigHeader from "../../components/common/DigHeader";
 import Layout from "../../components/common/layouts/Layout";
@@ -20,6 +21,17 @@ class Release extends Component {
     return (
       <Layout header={header}>
         <h1>{`${release.artist.name} - ${release.title}`}</h1>
+        <div className="columns">
+          <div className="column is-one-third">
+            <img
+              src={`/static/files/releases/${release.filename}`}
+              alt={`${release.artist.name} - ${release.title}`}
+            />
+          </div>
+          <div className="column">
+            <Markdown>{release.description}</Markdown>
+          </div>
+        </div>
       </Layout>
     );
   }
