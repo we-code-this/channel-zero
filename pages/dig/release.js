f71fab976acbf8fd20380ae6b01a0c2efb5d77f0
Michael Gregoire <mgregoire@nvzion.com>
2019-01-27 20:32:35 -0500

    More development of dig/release

--- pages/dig/release.js
+++ pages/dig/release.js
@@ -67,11 +67,50 @@ class Release extends Component {
               </div>
               <div className="card-content">
                 <div className="content">
+                  <ul class="is-unstyled">
+                    <li>
+                      <strong>Executive Producer:</strong> Chuck D
+                    </li>
+                    <li>
+                      <strong>Album Producer:</strong> C-Doc
+                    </li>
+                    <li>
+                      <strong>Associate Producer:</strong> JP Hesser of Castaway
+                      7
+                    </li>
+                    <li>
+                      <strong>Mastered by:</strong>{" "}
+                      <a href="https://defexperience.com">
+                        Shawn Franklin at “S.T.O.H.” Labs for TDX (The
+                        Definitive Xperience)
+                      </a>
+                    </li>
+                    <li>
+                      <strong>Cover Photo by:</strong> Carl Ryder
+                    </li>
+                    <li>
+                      <strong>Additional Photos by:</strong> Eitan Miskevich
+                    </li>
+                    <li>
+                      <strong>Cover Design by:</strong> Kelvin Fonville
+                    </li>
+                    <li>
+                      <strong>Additional Design and Layout by:</strong> DC
+                      Snyder
+                    </li>
+                  </ul>
+                  <hr />
                   <p>
                     <span class="album-review">
                       “Incredible album! Not to be missed!”
                     </span>{" "}
                     <span class="album-reviewer">- Someone</span>
+                  </p>
+                  <p>
+                    <span class="album-review">
+                      “Among my favorite albums of the year!”
+                    </span>{" "}
+                    <span class="album-reviewer">- Another Person</span>
                   </p>
                 </div>
               </div>
