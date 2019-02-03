11a632e8079941584670e263029efe2477ffea99
Michael Gregoire <mgregoire@nvzion.com>
2019-02-03 15:57:01 -0500

    Break Release card out to component

--- pages/dig/release.js
+++ pages/dig/release.js
@@ -4,6 +4,7 @@ import { withRouter } from "next/router";
 import DigHeader from "../../components/common/DigHeader";
 import Layout from "../../components/common/layouts/Layout";
 import Vendors from "../../components/common/Vendors";
+import ReleaseCard from "../../components/common/ReleaseCard";
 import { findBySlug } from "../../models/Release/releases";
 
 class Release extends Component {
@@ -25,65 +26,7 @@ class Release extends Component {
         <h2 className="subtitle">{release.artist.name}</h2>
         <div className="columns">
           <div className="column is-half-tablet is-one-third-desktop">
-            <div className="card">
-              <div className="card-image">
-                <figure className="image">
-                  <img
-                    src={`/static/files/releases/${release.filename}`}
-                    alt={`${release.artist.name} - ${release.title}`}
-                  />
-                </figure>
-              </div>
-              <div className="card-content">
-                <div className="content">
-                  <ul class="is-unstyled">
-                    <li>
-                      <strong>Executive Producer:</strong> Chuck D
-                    </li>
-                    <li>
-                      <strong>Album Producer:</strong> C-Doc
-                    </li>
-                    <li>
-                      <strong>Associate Producer:</strong> JP Hesser of Castaway
-                      7
-                    </li>
-                    <li>
-                      <strong>Mastered by:</strong>{" "}
-                      <a href="https://defexperience.com">
-                        Shawn Franklin at “S.T.O.H.” Labs for TDX (The
-                        Definitive Xperience)
-                      </a>
-                    </li>
-                    <li>
-                      <strong>Cover Photo by:</strong> Carl Ryder
-                    </li>
-                    <li>
-                      <strong>Additional Photos by:</strong> Eitan Miskevich
-                    </li>
-                    <li>
-                      <strong>Cover Design by:</strong> Kelvin Fonville
-                    </li>
-                    <li>
-                      <strong>Additional Design and Layout by:</strong> DC
-                      Snyder
-                    </li>
-                  </ul>
-                  <hr />
-                  <p>
-                    <span class="album-review">
-                      “Incredible album! Not to be missed!”
-                    </span>{" "}
-                    <span class="album-reviewer">- Someone</span>
-                  </p>
-                  <p>
-                    <span class="album-review">
-                      “Among my favorite albums of the year!”
-                    </span>{" "}
-                    <span class="album-reviewer">- Another Person</span>
-                  </p>
-                </div>
-              </div>
-            </div>
+            <ReleaseCard release={release} />
           </div>
           <div className="column">
             <Vendors vendors={release.vendors} />
