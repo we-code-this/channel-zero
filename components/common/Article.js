7285c5e29029a92a90bc5ee8924a71ca82404651
Mike Gregoire <mgregoire@nvzion.com>
2019-08-07 08:45:12 -0400

    Update Article component to not use url

--- components/common/Article.js
+++ components/common/Article.js
@@ -1,17 +1,19 @@
 import { Columns } from "react-bulma-components";
 
-const Article = props => (
-  <Columns.Column className="article">
-    <div className="column-content">
-      <h2 className="title is-4">
-        <a href={props.url}>{props.title}</a>
-      </h2>
-      <p>
-        {props.summary.replace(/^(.{200}[^\s]*).*/, "$1…")}{" "}
-        <a href={props.url}>Read more…</a>
-      </p>
-    </div>
-  </Columns.Column>
-);
+const Article = props => {
+  return (
+    <Columns.Column className="article">
+      <div className="column-content">
+        <h2 className="title is-4">
+          <a href="#">{props.title}</a>
+        </h2>
+        <p>
+          {props.summary.replace(/^(.{200}[^\s]*).*/, "$1…")}{" "}
+          <a href="#">Read more…</a>
+        </p>
+      </div>
+    </Columns.Column>
+  );
+};
 
 export default Article;
