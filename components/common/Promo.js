72a953e76a2b02985e1d8a789eced0f2ac6fe7b8
Mike Gregoire <mgregoire@nvzion.com>
2020-01-05 13:40:49 -0500

    Update Promo to not link when url is #

--- components/common/Promo.js
+++ components/common/Promo.js
@@ -1,18 +1,25 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const Promo = props => {
-  const classes = props.className ? `${props.className} promo` : "promo";
+  const classes = props.className
+    ? `${props.className} promo`
+    : 'promo';
 
-  return (
-    <Link href={props.url}>
-      <a className={classes}>
-        <img
-          src={props.image_url}
-          alt={props.name}
-        />
-      </a>
-    </Link>
-  );
+  if (props.url !== '#') {
+    return (
+      <Link href={props.url}>
+        <a className={classes}>
+          <img src={props.image_url} alt={props.name} />
+        </a>
+      </Link>
+    );
+  } else {
+    return (
+      <span className={classes}>
+        <img src={props.image_url} alt={props.name} />
+      </span>
+    );
+  }
 };
 
 export default Promo;
