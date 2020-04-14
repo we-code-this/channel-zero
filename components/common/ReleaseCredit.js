546ee09452a9f22c456dd84c32bd3ee427679abe
Mike Gregoire <mgregoire@nvzion.com>
2020-04-14 18:14:32 -0400

    Decode html characters in credit and tracklists

--- components/common/ReleaseCredit.js
+++ components/common/ReleaseCredit.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React from 'react';
+import he from 'he';
 
-const ReleaseCredit = props => {
-  const value = props.url ? <a href={props.url}>{props.value}</a> : props.value;
+const ReleaseCredit = (props) => {
+  const value = props.url ? (
+    <a href={props.url}>{he.decode(props.value)}</a>
+  ) : (
+    he.decode(props.value)
+  );
 
   return (
     <React.Fragment>
-      <strong>{props.label}:</strong> {value}
+      <strong>{he.decode(props.label)}:</strong> {value}
     </React.Fragment>
   );
 };
--- components/common/Track.js
+++ components/common/Track.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import he from 'he';
 
-const Track = props => (
+const Track = (props) => (
   <div className="track">
     <span className="track-number">{props.number} </span>
-    <span className="track-title">{props.title}</span>
+    <span className="track-title">{he.decode(props.title)}</span>
   </div>
 );
 
